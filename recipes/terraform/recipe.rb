a8ed3ac3518429accb2895f6f12621033b58c4cd
Dean Wilson <deanwilson@gmail.com>
2016-04-14 17:57:11 +0100

    Fix indentation
    Fix indentation

--- recipes/terraform/recipe.rb
+++ recipes/terraform/recipe.rb
@@ -6,7 +6,7 @@ class Goss < FPM::Cookery::Recipe
   name    'terraform'
   version '0.6.14'
   license 'MPLv2.0'
-  arch     'amd64'
+  arch    'amd64'
 
   source "https://releases.hashicorp.com/terraform/#{version}/terraform_#{version}_linux_amd64.zip"
   sha256 '6d93290f980df15a453e907ea9a2ca8f8fed41143c220953c911b5174c3e3ab0'
