93576d0191cb2523ac7cdf8901d0a8ae140228a9
Dean Wilson <dean.wilson@gmail.com>
2018-03-10 15:16:15 +0000

    Add a recipe to package json2hcl

---
+++ json2hcl/recipe.rb
@@ -0,0 +1,25 @@
+class Json2Hcl < FPM::Cookery::Recipe
+  description 'Convert JSON to HCL, and vice versa'
+  homepage    'https://github.com/kvz/json2hcl'
+  maintainer  'Dean Wilson <dean.wilson@gmail.com>'
+
+  name    'json2hcl'
+  version '0.0.6'
+  license 'MIT'
+  arch    'amd64'
+
+  source "https://github.com/kvz/json2hcl/releases/download/v#{version}/json2hcl_v#{version}_linux_amd64"
+  sha256 'd124ed13f3538c465fcab19e6015d311d3cd56f7dc2db7609b6e72fec666482d'
+
+  def build; end
+
+  def install
+    target_dir = "#{destdir}/usr/local/bin"
+
+    # make a reusable name to make the cp easier to read
+    file_name = "json2hcl_v#{version}_linux_amd64"
+
+    safesystem "mkdir -p #{target_dir}"
+    safesystem "cp #{builddir}/#{file_name}/#{file_name} #{target_dir}/json2hcl"
+  end
+end
