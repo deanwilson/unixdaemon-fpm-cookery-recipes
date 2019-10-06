fa774f00463dec9c39acf98b1fd1939155c3ec22
Dean Wilson <dean.wilson@gmail.com>
2019-10-06 11:19:50 +0100

    Bump to newer version

--- terraform/recipe.rb
+++ terraform/recipe.rb
@@ -4,12 +4,12 @@ class Terraform < FPM::Cookery::Recipe
   maintainer  'Dean Wilson <dean.wilson@gmail.com>'
 
   name    'terraform'
-  version '0.11.8'
+  version '0.12.9'
   license 'MPLv2.0'
   arch    'amd64'
 
   source "https://releases.hashicorp.com/terraform/#{version}/terraform_#{version}_linux_amd64.zip"
-  sha256 '84ccfb8e13b5fce63051294f787885b76a1fedef6bdbecf51c5e586c9e20c9b7'
+  sha256 '69712c6216cc09b7eca514b9fb137d4b1fead76559c66f338b4185e1c347ace5'
 
   def build; end
 
